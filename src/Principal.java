af984cc0a91db957b2f28e3a8b3965566e2e8267
sidneyperota <sidneyperota@gmail.com>
2020-01-20 17:34:56 -0300

    preparacao post

--- src/Principal.java
+++ src/Principal.java
@@ -1,21 +1,60 @@
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 
 public class Principal {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	
+	public static void enviar() {
+		
+		
+		HttpClient httpclient = HttpClients.createDefault();
+        
+		HttpPost httppost = new HttpPost("http://localhost:8080/teste.php/funcionario/gravar");
+		         
+		try { 
+		    ArrayList<NameValuePair> valores = new ArrayList<NameValuePair>();
+		    valores.add(new BasicNameValuePair("nome", "rodrigo"));
+		    valores.add(new BasicNameValuePair("sobrenome", "aramburu"));
+		             
+		    httppost.setEntity( new UrlEncodedFormEntity( valores ) );
+		    HttpResponse response = httpclient.execute( httppost );
+		              
+		    HttpEntity entity = response.getEntity();
+		    String content = EntityUtils.toString(entity);
+		    System.out.println( content );
+		             
+		} catch (ClientProtocolException e) {
+		    e.printStackTrace();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		} finally {
+		    httppost.releaseConnection();;
+		}
+		
+		
+		
+	}
+	
+	
+	
+	public static void requisitar() { 
 		
 		HttpClient httpclient = HttpClients.createDefault();
 		//HttpGet httpget = new HttpGet("http://www.botecodigital.info/exemplos/teste_requisicao.php");
@@ -43,24 +82,12 @@ public class Principal {
 		            System.out.println( scan.nextLine() );
 		        } 
 		  }
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
+    
 		    
 		    System.out.print( status.toString() ); 
+		    
+		    
+ 
 		 
 		   // System.out.print( response.toString() ); 
 		 
@@ -71,6 +98,22 @@ public class Principal {
 		} finally {
 		    httpget.releaseConnection();;
 		}
+		
+		
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		enviar();
+		
+		
+		//requisitar(); 
+		
+		
 		
 	}
 
