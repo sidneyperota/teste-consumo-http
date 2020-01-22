6dda32aee66a2f1d4035ca2b5c4a2de8ce71930f
sidneyperota <sidneyperota@gmail.com>
2020-01-22 11:12:47 -0300

    leitura_json

--- src/ManipulaJSon.java
+++ src/ManipulaJSon.java
@@ -1,64 +1,65 @@
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
-import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
-
-public class Principal {
+public class ManipulaJSon {
 	
 	
-	public static void enviar() {
+	public void criarJSon() { 
+
+		JSONObject jsonObjeto;
 		
+		List lista = new LinkedList(); 
 		
-		HttpClient httpclient = HttpClients.createDefault();
-        
-		HttpPost httppost = new HttpPost("http://localhost:8080/teste.php/funcionario/gravar");
-		         
-		try { 
-		    ArrayList<NameValuePair> valores = new ArrayList<NameValuePair>();
-		    valores.add(new BasicNameValuePair("nome", "rodrigo"));
-		    valores.add(new BasicNameValuePair("sobrenome", "aramburu"));
-		             
-		    httppost.setEntity( new UrlEncodedFormEntity( valores ) );
-		    HttpResponse response = httpclient.execute( httppost );
-		              
-		    HttpEntity entity = response.getEntity();
-		    String content = EntityUtils.toString(entity);
-		    System.out.println( content );
-		             
-		} catch (ClientProtocolException e) {
-		    e.printStackTrace();
-		} catch (IOException e) {
-		    e.printStackTrace();
-		} finally {
-		    httppost.releaseConnection();;
-		}
+		jsonObjeto = new JSONObject();
+		jsonObjeto.put("codigo", "1"); 
+		jsonObjeto.put("nome","sidney francisco");
+		lista.add( jsonObjeto ); 
 		
+		jsonObjeto = new JSONObject();
+		jsonObjeto.put("codigo", "2"); 
+		jsonObjeto.put("nome","jose de souza");
 		
+		lista.add( jsonObjeto );
+		
+		String sJson = JSONValue.toJSONString(lista); 
+		
+		lista = (List) JSONValue.parse( sJson );
+		
+		jsonObjeto = (JSONObject) lista.get(1);
+		
+		System.out.print( jsonObjeto.toString() );
+		
+		//System.out.println(sJson);
+
+		//System.out.print( jsonObjeto.toString() ); 
 		
 	}
 	
 	
-	public static void requisitar() { 
+	public String obterJSon() { 
 		
 		HttpClient httpclient = HttpClients.createDefault();
 		//HttpGet httpget = new HttpGet("http://www.botecodigital.info/exemplos/teste_requisicao.php");
 		
 		HttpGet httpget = new HttpGet("http://localhost:8080/api.php");	
+		
+		String sJSon = null; 
 		
 	
 		try {
@@ -78,13 +79,15 @@ public class Principal {
 		        Scanner scan = new Scanner( in );
 		        
 		        while( scan.hasNext() ){
-		            System.out.println( scan.nextLine() );
+		            //System.out.println( scan.nextLine() );
+		            sJSon = scan.nextLine(); 
 		        } 
 		  }
     
 		    
 		    //System.out.print( status.toString() ); 
 
+		    
 		 
 		   // System.out.print( response.toString() ); 
 		 
@@ -97,26 +100,30 @@ public class Principal {
 		}
 		
 		
+		return sJSon;
+
+	}
+	
+
+	public void converterJSon() { 
+
+		JSONObject jsonObjeto;
+		
+		List lista = new LinkedList(); 
+		
+		String sJson; 
+		sJson = obterJSon();
+		
+		lista = (List) JSONValue.parse( sJson );
+		
+		jsonObjeto = (JSONObject) lista.get(1);
+		
+		System.out.print( jsonObjeto.toString() );
+		
 	}
 	
 	
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
-		//enviar();
-		
-		//requisitar();
-		
-		ManipulaJSon manipulaJSon = new ManipulaJSon();
-		//manipulaJSon.criarJSon();
-
-		
-		manipulaJSon.converterJSon(); 
-
-		
-		
-	}
+	
+	
 
 }
